82684f5d438bdd84570bfb06444f4e0fc610dbb6
unknown <yaalu18@gmail.com>
2024-08-31 19:20:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomVideoPlayer from './pages/CustomVideoplayer';
+import CustomVideoPlayer1 from './pages/CustomVideoplayer1';
+import CustomVideoPlayer3 from './pages/CustomVideoplayer3';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello
+     <div className="App">
+      <h1>Custom Video Player</h1>
+      {/*<CustomVideoPlayer />*/}
+      <CustomVideoPlayer3/>
+    </div>
     </div>
   );
 }
